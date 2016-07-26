c9633c8e44472c9c3980fc8b1b02cc6aa5771d2b
root <root@swarm-manager1>
2016-07-26 11:35:51 +0200

    first commit

---
+++ entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/sh -x
+
+ACTION=$1
+
+DNSSERVER="${DNSSERVER:=8.8.8.8}"
+
+DATADIR="${DATADIR:=/data}"
+
+SERVER="${SERVER:=false}"
+
+
+[ -n "${ADVERTISEIP}" ] && ADVERTISEIP="-advertise ${ADVERTISEIP}"
+
+[ -n "${DATACENTER}" ] && DATACENTER="-dc ${DATACENTER}"
+
+[ -n "${NAME}" ] && NAME="-node ${NAME}"
+
+
+GetFirstIP(){
+	firstip="$(ip add show eth0 |awk '/inet / { print $2 }'|cut -d "/" -f1)"
+}
+
+
+StartConsulAsServer(){
+	GetFirstIP
+	[ ! -f ${DATADIR} ] && mkdir -p ${DATADIR}
+	SERVER="-server -bootstrap "
+	CMD="/apps/consul agent ${ADVERTISEIP} ${NAME} ${SERVER} ${DATACENTER}\
+	-data-dir=${DATADIR} -client $firstip -recursor=${DNSSERVER}"
+
+
+	echo "CMD: ${CMD}"
+	${CMD}
+
+}
+
+case $ACTION in
+	start|START)
+		[ "$SERVER" = "true" ] && StartConsulAsServer
+	;;
+
+	*)
+		exec "$@"
+	;;
+
+esac
